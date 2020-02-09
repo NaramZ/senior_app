e04476269bd35a81db1e4bdc64a43f6144a0de81
Naram <Naram.Ziady@gmail.com>
2020-02-09 23:39:42 +0200

    a bunch of shit
    added md bootstrap
    added images
    added shit load of shit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav';
 import NavLink from 'react-bootstrap/NavLink';
-
+import Image from 'react-bootstrap/Image';
+import profilePic from '../../assets/profilePic.jpg'
+import classes from './NavBar.module.css';
 class Navigation extends Component{
 render(){
     return (
@@ -10,14 +12,19 @@ render(){
             <Navbar.Brand href="#home">Poison Picker</Navbar.Brand>
             <Navbar.Toggle aria-controls = 'basic-navbar-nav'/>
             <Navbar.Collapse id="basic-navbar-nav">
-
                 <Nav>
                     <NavLink className = 'd-inline p-2 bg-white text-black'
-                    to = '/'>Home</NavLink>
+                    to = '/Home'>Home</NavLink>
                     <NavLink className = 'd-inline p-2 bg-white text-black'
                     to = '/Favorites'>Favorites</NavLink>
                     
                 </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+                 <Navbar.Text>
+                        {/* <Image src={profilePic}  className= {classes.image}/>  */}
+                        <a href="#login">Naram Ziady</a>
+                </Navbar.Text>
             </Navbar.Collapse>
         </Navbar>
     )
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import classes from './App.module.css';
+
+import classes from './App.module.css'
 import Image from 'react-bootstrap/Image'
 import Aux from '../hoc/Aux';
 import wImage from '../assets/Whiskey.jpg';
 import tImage from '../assets/Tequila.png';
 import Navigation from '../components/NavBar/NavBar';
+import TestimonialsPage from '../components/Test';
 
 function App() {
 
   return (
     <Aux>
       <Navigation/>
+      <TestimonialsPage>
+
+      </TestimonialsPage>
       <div className={classes.drinkbox}>
         <Image src={wImage} rounded className={classes.img}/> Whiskey </div>
         <div className={classes.drinkbox}>
