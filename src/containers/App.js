bca1081ada26926bfdac5a401701eb0b753d6f1e
Naram <Naram.Ziady@gmail.com>
2020-02-09 16:35:48 +0200

    12

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import classes from './App.module.css';
-import Aux from '../hoc/Aux';
 import Image from 'react-bootstrap/Image'
+import Aux from '../hoc/Aux';
 import wImage from '../assets/Whiskey.jpg';
 import tImage from '../assets/Tequila.png';
 import Navigation from '../components/NavBar/NavBar';
-import FormFinal from '../components/Form/Form';
 
 function App() {
 
   return (
-    <div>
+    <Aux>
       <Navigation/>
       <div className={classes.drinkbox}>
         <Image src={wImage} rounded className={classes.img}/> Whiskey </div>
         <div className={classes.drinkbox}>
         <Image src={tImage} rounded className={classes.img}/> Tequila  </div>
-    </div>
+    </Aux>
     
   );
 }
