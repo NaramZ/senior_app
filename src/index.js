6d53131da05a93fcc25586d6c4755aeb584cc929
Naram <Naram.Ziady@gmail.com>
2020-02-13 18:59:49 +0200

    roboto

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import 'typeface-roboto';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
