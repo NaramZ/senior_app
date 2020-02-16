8e230acf30f8792878ad962627e2f6b2169678c1
Naram <Naram.Ziady@gmail.com>
2020-02-16 22:53:57 +0200

    idk

--- src/components/Utilities/Card/Card.styled.js
+++ src/components/Utilities/Card/Card.styled.js
@@ -8,18 +8,16 @@ left: 5%;
 display: flex;
 width: 25rem;
 height: 15rem;
-background : ${({theme}) => theme.primaryDark};
-color: ${({ theme }) => theme.primaryDark};
 border: none;
 border-radius:25px;
 cursor: pointer;
 padding: 0;
-
-
-
+margin:20px;
 z-index: 0;
 box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+background : ${({theme}) => theme.primaryDark};
+color: ${({ theme }) => theme.primaryDark};
 
 img {
   height:100%;
