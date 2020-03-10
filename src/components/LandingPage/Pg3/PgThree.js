a8eda191f24dfaa9e5b185e65d8e538eaa8e8a0f
Naram <Naram.Ziady@gmail.com>
2020-03-10 20:56:28 +0200

    fixing pg 3

--- src/components/LandingPage/Pg3/PgThree.js
+++ src/components/LandingPage/Pg3/PgThree.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Aux from './../../../hoc/Aux';
 import {StyledPgThree} from './PgThree.styled';
-
+import DrinkImg from '../../../assets/drink.jpg'
 function PgThree() {
     return (
     <Aux>
@@ -9,6 +9,15 @@ function PgThree() {
             <ul>
                 <li>
                     <h1>Drinks</h1>
+                </li>
+                <li>
+                    <h5>Choose for yourself the Drinks that you want</h5>
+                </li>
+                <li>
+                    <div>
+                        <h2>Mohito</h2>
+                        <img src = {DrinkImg} />
+                    </div>
                 </li>
             </ul>
         </StyledPgThree>
--- src/components/LandingPage/Pg3/PgThree.styled.js
+++ src/components/LandingPage/Pg3/PgThree.styled.js
@@ -16,28 +16,38 @@ ul{
         }
         :nth-child(2) {
         z-index:1;
-        grid-column: 5/8;
-        grid-row: 2;
+        grid-column: 2/5;
+        grid-row: 5;
         }
         :nth-child(3){
             z-index:1;
-            grid-column: 4/9;
-            grid-row-start: 3;
-            text-align: center;
+            grid-column: 5/9;
+            grid-row: 3/8;
         }
     }
     div {
         display: inline-block;
         position: absolute;
         overflow: hidden;
-        width: 100%;
-        height: 60%;
+        width: 30rem;
+        height: 30rem;
+        border: 1.2px solid #FCD561;
+        border-radius: 0 24px;
+        text-align: center;
+        h2{
+        font-size: 3em;
+        letter-spacing: 8px;
+        margin:0;
+        font-family: "Roboto Condensed";
+    }
     }
     img {
-        position: absolute;
         display: inline-block;
-        top: -650px;
+        position: absolute;
         width: 100%;
+        height: 100%;
+        right: 6px;
+        z-index: 1;
     }
     
 
@@ -46,11 +56,13 @@ ul{
         letter-spacing: 8px;
         display: inline-block;
         margin: 0;
+        z-index: 10;
         font-family: 'Roboto Condensed';
         :hover {
             text-shadow: 10px 10px 0px rgba(251, 109, 58, 0.6);
         }
     }
+    
     h5{
         font-family: 'Roboto Light';
         display: inline-block;
