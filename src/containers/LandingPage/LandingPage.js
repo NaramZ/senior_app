e77025d13ada5d0d720c52adc27c7d0c5106dcd8
Naram <Naram.Ziady@gmail.com>
2020-03-16 13:22:18 +0200

    Removed styled components for Pg2 as no need for them

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import LandingNav from '../../components/LandingPage/LandingBar';
 import PgOne from '../../components/LandingPage/Pg1/PageOne';
-import PgTwo from '../../components/LandingPage/Pg2';
+import PgTwo from '../../components/LandingPage/Pg2/PageTwo';
 import PgThree from '../../components/LandingPage/Pg3';
 import PgFour from '../../components/LandingPage/PgFour';
 function App() { 
